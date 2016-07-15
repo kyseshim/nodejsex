f9d70e39384104c5ec9f81e3b142ace7debb64d8
kyseshim <kyseshim5899@naver.com>
2016-07-16 00:57:19 +0900

    first-day commit
    http server default setup

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var http = require('http');
-http.createServer(function(req,res) {
-	res.writeHead(200, {
+http.createServer(function(request,response) {
+	response.writeHead(200, {
 	'Content-type' : 'text/plain'
 	});
-	res.end('Hello world ssiballlllllll jotgata!!!! mother! save me please!');
+	response.end('Hello world ssiballlllllll jotgata!!!! mother! save me please!');
 }).listen(3000);
 
 console.log('Server is running at http://localhost:3000');
