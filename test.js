9e82072b2c1a38a3c070ce41ec88bf8f9b5b063f
kyseshim <kyseshim5899@naver.com>
2016-07-16 00:45:02 +0900

    first-day commit
    node.js architect test

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ http.createServer(function(req,res) {
 	res.writeHead(200, {
 	'Content-type' : 'text/plain'
 	});
-	res.end('Hello world ssiballlllllll jotgata!!!! mother!');
+	res.end('Hello world ssiballlllllll jotgata!!!! mother! save me please!');
 }).listen(3000);
 
 console.log('Server is running at http://localhost:3000');
