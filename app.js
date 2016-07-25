d208fce1d6a533d246867a2beebee45bca5a04fc
kyseshim <kyseshim5899@naver.com>
2016-07-25 23:39:39 +0900

    modifying joining page(in progress)

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+
+
 var mongoose = require('mongoose');
 var http = require('http');
 var express = require('express');
 var fs = require('fs');
 var app = express();
 
-/*
+
 //기본 셋업
 //http.createServer(function(request,response) {
 	//response.writeHead(200, {
 	//'Content-type' : 'text/plain'
 	//});
 	//response.end('Hello world ssiballlllllll jotgata!!!! ssiballlllllll mother! save me please!');
-}).listen(3000);
-*/
+//}).listen(3000);
+
 
 app.listen(3000, function(){
 	console.log('server start');
@@ -20,13 +22,13 @@ app.listen(3000, function(){
 
 //메인페이지에서 접속할 때 페이지
 app.get('/', function(require, response){
-	fs.readFile('./html/index.html', function(err, data){
+	fs.readFile('./public/index.html', function(err, data){
 		response.writeHead(200, {'Content-type': 'text/html'});
 		response.end(data);
 	})
 } )
 app.get('/main', function(require, response) {
-	fs.readFile('./html/main.html', function(err, data){
+	fs.readFile('./public/main.html', function(err, data){
 		response.writeHead(200, { 'Content-type': 'text/html'});
 		response.end(data);
 	});
@@ -61,4 +63,5 @@ Data.findOne({name:"myData", count:0}, function(err, data){
 	}
 });
 
-console.log('Server is running at http://localhost:3000');
+//console.log('Server is running at http://localhost:3000');
+
